9eb60ef85af73356429de5a40e80cfde388db0e0
DangQuangSE <quangndse180246@fpt.edu.vn>
2026-07-24 12:51:18 +0700

    init

---
+++ src/test/java/com/pte/api/PteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pte.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
