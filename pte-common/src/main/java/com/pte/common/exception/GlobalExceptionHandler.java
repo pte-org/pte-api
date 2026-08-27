96ae24a51bcf528286de3dcd100cd784d25a122f
Lê Nguyễn An Ninh <leninh2004@gmail.com>
2026-08-27 19:51:41 +0700

    feat: add logging for unhandled exceptions in GlobalExceptionHandler

--- pte-common/src/main/java/com/pte/common/exception/GlobalExceptionHandler.java
+++ pte-common/src/main/java/com/pte/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.pte.common.exception;
 
 import com.pte.common.web.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     private static final String VALIDATION_FALLBACK = "VALIDATION_ERROR";
     private static final String INTERNAL_ERROR = "INTERNAL_ERROR";
@@ -33,6 +37,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleUnexpected(Exception ex) {
+        log.error("Unhandled exception", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error(INTERNAL_ERROR));
     }
 }
